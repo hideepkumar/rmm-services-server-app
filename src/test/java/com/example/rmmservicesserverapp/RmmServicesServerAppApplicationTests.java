e32b9b062aa4e2d3adb8a7520f25c92eaaf2b7d9
Deep <deepk.online@gmail.com>
2021-07-27 14:57:37 -0700

    Initial commit

---
+++ src/test/java/com/example/rmmservicesserverapp/RmmServicesServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rmmservicesserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmmServicesServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
